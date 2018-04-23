046b62de78ad64d4d5d44d6d80f88d7d23cb035e
bmclela <35577958+bmclela@users.noreply.github.com>
2018-04-23 19:34:10 -0400

    Update setup-controller.sh

--- setup-controller.sh
+++ setup-controller.sh
@@ -4459,7 +4459,7 @@ openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id},ip-
 openstack port create --network ${network_id} --fixed-ip subnet=${subnet_id},ip-address=10.11.10.24 testport4
 
 # See https://docs.openstack.org/project-install-guide/baremetal/draft/configure-glance-images.html
-wget -O /tmp/setup/OL7.vmdk https://clemson.box.com/s/allbu0tn8l6opdr553ce0zwa3c591pja
+wget -O /tmp/setup/OL7.vmdk https://clemson.box.com/s/f2ixde6mqkdgf56xq80t3wfmgcif2xsd
 glance image-create --name OL7 --disk-format vmdk --visibility public --container-format bare < /tmp/setup/OL7.vmdk
 
 project_id=`openstack project list -f value | grep admin | cut -d' ' -f 1`
